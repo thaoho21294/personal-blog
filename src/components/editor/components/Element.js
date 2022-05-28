58f78354756ef7dda689eccdd7884709f929ff43
Thao <thaoho21294@gmail.com>
2022-05-28 15:48:05 +0700

    Fix alignment didn't work

--- src/components/editor/components/Element.js
+++ src/components/editor/components/Element.js
@@ -4,15 +4,35 @@ const Element = ({ attributes, children, element }) => {
   const style = { textAlign: element.align }
   switch (element.type) {
     case 'paragraph':
-      return <p {...attributes}>{children}</p>
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      )
     case 'h1':
-      return <h1 {...attributes}>{children}</h1>
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      )
     case 'h2':
-      return <h2 {...attributes}>{children}</h2>
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      )
     case 'h3':
-      return <h3 {...attributes}>{children}</h3>
+      return (
+        <h3 style={style} {...attributes}>
+          {children}
+        </h3>
+      )
     case 'h4':
-      return <h4 {...attributes}>{children}</h4>
+      return (
+        <h4 style={style} {...attributes}>
+          {children}
+        </h4>
+      )
     case 'block-quote':
       return (
         <blockquote style={style} {...attributes}>
@@ -24,18 +44,6 @@ const Element = ({ attributes, children, element }) => {
         <ul style={style} {...attributes}>
           {children}
         </ul>
-      )
-    case 'heading-one':
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      )
-    case 'heading-two':
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
       )
     case 'list-item':
       return (
