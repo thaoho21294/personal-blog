4cd396dae60398ee44e021c9ad5d1898ffada96e
Thao <thaoho21294@gmail.com>
2022-07-13 15:12:13 +0700

    Test mongo db connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// require('dotenv').config()
+require('dotenv').config()
 
 router.get('/', async (req, res) => {
   try {
@@ -17,13 +17,13 @@ router.get('/', async (req, res) => {
   }
 })
 
-// const uri = process.env.ATLAS_URI
-// mongoose.connect(uri, { useNewUrlParser: true, autoIndex: true })
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, autoIndex: true })
 
-// const connection = mongoose.connection
-// connection.once('open', () => {
-//   console.log('MongoDB database connection established successfully')
-// })
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully')
+})
 
 app.use(express.json({ extended: false }))
 
