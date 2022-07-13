bb8d834aea86ba016c60a61bea423f03e550a1b5
Thao <thaoho21294@gmail.com>
2022-07-13 15:10:41 +0700

    Test deploymennt

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+// const mongoose = require('mongoose')
+
+// require('dotenv').config()
 
 router.get('/', async (req, res) => {
   try {
@@ -13,6 +16,14 @@ router.get('/', async (req, res) => {
     return res.status(500).send('Server error')
   }
 })
+
+// const uri = process.env.ATLAS_URI
+// mongoose.connect(uri, { useNewUrlParser: true, autoIndex: true })
+
+// const connection = mongoose.connection
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully')
+// })
 
 app.use(express.json({ extended: false }))
 
