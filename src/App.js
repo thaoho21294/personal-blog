dc8933d89a58d34584fad35b351ef93c50b51268
Tung Le <tungle251@gmail.com>
2022-08-09 15:16:13 +0700

    rename

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from 'scenes/login'
 import Home from 'scenes/home'
-import Resume from './scenes/resume'
+import Resume from 'scenes/resume_test'
 import './App.scss'
 
 function App() {
