a2a2b58b8195ec413aedd395de1e9e65919b81d8
Tung Le <tungle251@gmail.com>
2022-08-12 09:14:10 +0700

    minor fix

--- src/schema/loginValidationSchema.js
+++ src/schema/loginValidationSchema.js
@@ -1,9 +1,8 @@
-import * as yup from 'yup'
+import { object, string } from 'yup'
 
-const loginValidationSchema = yup.object({
-  username: yup.string().required('Username is required'),
-  password: yup
-    .string()
+const loginValidationSchema = object({
+  username: string().required('Username is required'),
+  password: string()
     .min(8, 'Password should be of minimum 8 characters length')
     .required('Password is required'),
 })
